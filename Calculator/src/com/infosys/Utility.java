cfcadb5960a5554d328bd721f76d7d94264f9e14
rohan <rohan@DB.com>
2015-01-19 03:39:43 +0530

    second changes

--- Calculator/src/com/infosys/Utility.java
+++ Calculator/src/com/infosys/Utility.java
@@ -9,6 +9,8 @@ public class Utility {
 		System.out.println("Addtion of 10 and 30= "+Addition.add(10, 30));
 		System.out.println("Subtraction of 30 and 10= "+Subtraction.sub(30,10));
 		System.out.println("OKK");
+		System.out.println("OKK done ");
+
 	}
 
 }
