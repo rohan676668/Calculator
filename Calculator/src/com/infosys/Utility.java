ab15d471ad7c7f6a8c2290e1efdbd4f87ba11240
rohan <rohan@DB.com>
2015-01-19 03:33:01 +0530

    First Changes on the rocks

--- Calculator/src/com/infosys/Utility.java
+++ Calculator/src/com/infosys/Utility.java
@@ -8,6 +8,7 @@ public class Utility {
 	public static void main(String[] args) {
 		System.out.println("Addtion of 10 and 30= "+Addition.add(10, 30));
 		System.out.println("Subtraction of 30 and 10= "+Subtraction.sub(30,10));
+		System.out.println("OKK");
 	}
 
 }
